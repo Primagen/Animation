96ccff114c08e52300ce5a1fedb3daf999dcaab6
Primagen <tprimagen@gmail.com>
2014-11-29 13:47:44 -0500

    WIP Animation
    Uploaded some basic stuff to encourage myself to upload more stuff later
    :)

---
+++ README.md
@@ -0,0 +1,10 @@
+Animation
+============
+
+The beginnings of a program used for the purpose of making animations.<br>
+Concentration on vector graphic rendering for the moment.<br>
+Currently uses the LodePNG library to handle PNG encoding and decoding.<br>
+Currently renders hundreds of frames that then can be compiled into a video using external programs/libraries.<br>
+Probably will add JPEG support for faster rendering<br>
+Will upload more stuff later...<br>
+Thank you, cplusplus.com<br>
---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * Header for organizing some of the constants
+ * that various parts of the program use
+ */
+namespace values {
+  const double Pi = 3.14159265358979323851280895940618620443274267017841339111328125;
+  const double Sqrt2 = 1.41421356237309504880168872420969807856967187537694807317667973799;
+}
+
+namespace strings {
+  const std::string Separate        ( "[Separate]"  );
+  const std::string Connected       ( "[Connected]" );
+  const std::string Line            ( "[Line]"      );
+  const std::string BezierQuadratic ( "[Bezier2]"   );
+  const std::string BezierCubic     ( "[Bezier3]"   );
+  const std::string Circle          ( "[Circle]"    );
+  const std::string Ellipse         ( "[Ellipse]"   );
+}
+
