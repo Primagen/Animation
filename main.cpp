18683d862525d02b8861531b98bd3852563f1742
Primagen <tprimagen@gmail.com>
2014-11-29 21:38:33 -0500

    main.cpp example
    Added an example where we create 1000 random bezier curves.

--- main.cpp
+++ main.cpp
@@ -17,3 +17,45 @@ namespace strings {
   const std::string Ellipse         ( "[Ellipse]"   );
 }
 
+
+#elif ANIMATION_TEST == 8
+/**
+ *  8. Vector graphics test #2
+ *   Creates and renders a bunch of random Quadratic Bezier Curves
+ */
+int main()
+{
+    srand(time(NULL));
+    const unsigned Width  = 1366; //500;
+    const unsigned Height =  768; //500;
+    const Coordinate c1(0, 0);
+    const Coordinate c2(Width/2, Height);
+    const Coordinate c3(3*Width/4, Height/4);
+
+    // Let's try this out...
+    ///std::vector<Coordinate> line1 = {c1, c2    };
+    ///std::vector<Coordinate> line2 = {c2, c3    };
+    ///std::vector<Coordinate> bezr1 = {c3, c2, c1};
+    
+    std::vector<VectorGraphic> graphics;
+    for (int i = 0; i < 1000; i++)
+    {
+        std::vector<Coordinate> random_bezier = {
+            Coordinate(rand()%(2*Width), 2*rand()%(2*Height)),
+            Coordinate(rand()%(2*Width), 2*rand()%(2*Height)),
+            Coordinate(rand()%(2*Width), 2*rand()%(2*Height))
+        };
+        //TODO: emplace_back
+        graphics.push_back(VectorGraphic(strings::BezierQuadratic, random_bezier));
+    }
+
+    // Specify what you want to add the list of coordinates as:
+    ///graphics.push_back(VectorGraphic(strings::Line,            line1));
+    ///graphics.push_back(VectorGraphic(strings::Line,            line2));
+    ///graphics.push_back(VectorGraphic(strings::BezierQuadratic, bezr1));
+
+    render("vector_images/vector_graphics_random.png", graphics, Width, Height, Color::Blue);
+    std::cout << "vector graphic rendered to file." << std::endl;
+}
+
+#endif // ANIMATION_TEST
